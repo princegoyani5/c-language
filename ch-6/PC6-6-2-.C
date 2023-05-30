ea989c5195cb1c5cf147fa0270b143d6fe401d2f
princegoyani5 <princegoyani5@gmail.com>
2023-05-30 19:01:30 +0530

    PG | 30/05/2023 | ch-6.1

---
+++ ch-6/PC6-6-2-.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int 1=n;
+	clrscr();
+
+	while(i>=1)
+	{
+		printf("%d",i);
+		i++;
+	getch();
+}
---
+++ ch-6/PC6-6-4.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,n;
+	clrscr();
+
+	printf("enter starting year : ");
+	scanf("%d",&i);
+	printf("enter ending year : ");
+	scanf("%d",&i);
+
+	while(i<=n)
+	{
+		if(i%4==0)
+		{
+
+			printf("%d",i);
+		}
+		i++;
+       }
+	getch();
+}
+	getch();
+}
