5f14ddfa7e8ff4ad80dcdaf93854fbcd17b02b53
princegoyani5 <princegoyani5@gmail.com>
2023-07-05 18:37:59 +0530

    |5/07/2023|ch-11

---
+++ ch-11/11-1-1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	int p;
+	printf("enetr array size : ");
+	scanf("%d",&p);
+	
+	int a[p],i;
+	int *b;
+	
+	b=&a;
+	
+	printf("enter array elements : \n");
+	for(i=0; i<p; i++)
+	{
+		printf("a[%d]: ",i);
+		scanf("%d",b+i);
+	}
+	printf("spuare of elements  :\n");
+	for(i=0; i<p; i++)
+	{
+		printf("%d ",*(b+i)**(b+i));
+	}
+	
+}
---
+++ ch-11/11-1-2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enetr the value of a : ");
+	scanf("%d",&a);
+	printf("enetr the value of b : ");
+	scanf("%d",&b);
+	
+	
+	int * a1,*a2;
+	a1=&a;
+	a2=&b;
+	
+	printf("before swap :\n a: %d \n b:%d \n",*a1,*a2);
+	*a1=*a1+*a2;
+	*a2=*a1+*a2;
+	*a1=*a1+*a2;
+	
+	printf("after swap :\n a: %d \n b:%d \n",*a1,*a2);
+	
+			
+}
